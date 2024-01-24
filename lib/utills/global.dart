8675763d7439e55eb56b89fc534bb70be7628435
vivek <Katariyavivek228@gmail.com>
2024-01-24 12:18:31 +0530

    Final Update

--- lib/utills/global.dart
+++ lib/utills/global.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+
 int i=1;
 String image="assets/image/1.png";
 String image2="assets/image/6.png";
@@ -9,3 +11,6 @@ int randomNumberMinMax(int min, int max){
   //generate random number within minimum and maximum value
   return randomminmax;
 }
+Color white=Colors.white;
+Color grey=Colors.blueGrey;
+bool color1=true;
