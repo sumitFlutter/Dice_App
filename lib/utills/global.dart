e7347732e0dfdb86d6251871bc4d29e743161b48
vivek <Katariyavivek228@gmail.com>
2024-01-24 10:43:36 +0530

    update

--- lib/utills/global.dart
+++ lib/utills/global.dart
@@ -2,4 +2,10 @@ import 'dart:math';
 
 int i=1;
 String image="assets/image/1.png";
-Random i1=Random();
+String image2="assets/image/6.png";
+int? min1, max1;
+int randomNumberMinMax(int min, int max){
+  int randomminmax = min + Random().nextInt(max - min);
+  //generate random number within minimum and maximum value
+  return randomminmax;
+}
